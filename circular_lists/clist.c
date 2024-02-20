bb61fccdf828eb7bcfacd26a08c992701a2707b8
Sheel2007 <62717403+Sheel2007@users.noreply.github.com>
2024-02-20 09:23:40 -0500

    Added clist files

---
+++ circular_lists/clist.c
@@ -0,0 +1,132 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "clist.h"
+
+Node* make_node(int data)
+{
+    Node* new = malloc(sizeof(Node));
+    if (new == NULL) {
+        printf("Memory allocation failed\n");
+        exit(1);
+    }
+    new->val = data;
+    new->next = NULL;
+
+    return new;
+}
+
+char* print_list(Node* head) {
+    char* result = malloc(1024);
+
+    int index = 0;
+    Node* node = head;
+    if (node == NULL) {
+        result[0] = '\0';
+        return result;
+    }
+    while (node != head || index == 0) {
+        int num_len = snprintf(NULL, 0, "%d", node->val);
+        snprintf(result + index, 8, "%d", node->val);
+        index += num_len - 1;
+        if (node->next != head) {
+            result[++index] = ' ';
+            result[++index] = '-';
+            result[++index] = '>';
+            result[++index] = ' ';
+        }
+        index++;
+
+        node = node->next;
+    }
+    result[index] = '\0';
+
+    return result;
+}
+
+void insert_at_end(Node** list, Node** newnode) {
+    // If the list is empty, make the new node the head of the list
+    if (*list == NULL) {
+        *list = *newnode;
+    } else {
+        // Traverse the list to find the last node
+        Node* current = *list;
+        while (current->next != NULL) {
+            current = current->next;
+        }
+        // Insert the new node at the end of the list
+        current->next = *newnode;
+    }
+}
+
+Node* remove_from_list(Node** list, int val) {
+    Node *current, *previous;
+    previous = *list;
+
+    if (previous == NULL) {     
+        return NULL;
+    }
+
+    current = previous->next;
+
+    // If list contains only one node
+    if (previous->next == previous && previous->val == val) {
+        *list = NULL;
+        free(previous);
+        return previous;
+    }
+
+    while (current != *list && current->val != val) {
+        previous = current;
+        current = current->next;
+    }
+
+    if (current == *list && current->val != val) { // Value not found
+        return NULL;
+    }
+
+    // If node to be removed is the head of the list
+    if (current == *list) {
+        previous->next = current->next;
+        *list = previous->next;
+    } else { // If node to be removed is not the head of the list
+        previous->next = current->next;
+    }
+
+    free(current);
+    return current;
+}
+
+void insert_at_index(Node** list, int val, int index) {
+    Node* new_node = make_node(val);
+
+    if (*list == NULL) { // If the list is empty, insert the node as the only node
+        *list = new_node;
+        new_node->next = new_node; // Circular reference to itself
+        return;
+    }
+
+    if (index <= 0) { // Insert at the beginning of the list
+        Node* last_node = *list;
+        while (last_node->next != *list) {
+            last_node = last_node->next;
+        }
+        last_node->next = new_node;
+        new_node->next = *list;
+        *list = new_node;
+        return;
+    }
+
+    // Traverse the list to find the node at the specified index
+    Node* current = *list;
+    for (int i = 0; i < index - 1; i++) {
+        current = current->next;
+        if (current == *list) {
+            printf("Index out of range\n");
+            return;
+        }
+    }
+
+    // Insert the node at the specified index
+    new_node->next = current->next;
+    current->next = new_node;
+}
