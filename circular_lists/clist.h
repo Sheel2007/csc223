00c596efec383b1a0153b7c7e1cb12f41602df85
Sheel2007 <62717403+Sheel2007@users.noreply.github.com>
2024-02-22 08:56:12 -0500

    Added Insert list in ascending order

--- circular_lists/clist.h
+++ circular_lists/clist.h
@@ -7,4 +7,5 @@ Node* make_node(int);
 Node* remove_from_list(Node**, int);
 void insert_at_end(Node**, Node**);
 char* print_list(Node*);
+void clist_insert_in_order(Node**, Node**);
 void insert_at_index(Node**, int, int);
